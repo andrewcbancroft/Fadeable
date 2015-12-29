1f2e234351d1be1de4acf0ff05caa5af454540a8
Andrew Bancroft <andrew@andrewcbancroft.com>
2015-12-29 16:38:17 -0600

    CocoaPods dependencies.

---
+++ Example/Pods/Target Support Files/Fadeable/Fadeable-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double FadeableVersionNumber;
+FOUNDATION_EXPORT const unsigned char FadeableVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-Fadeable_Example/Pods-Fadeable_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Fadeable_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Fadeable_ExampleVersionString[];
+
